915f57ceb163ed3238f68fa59da56e712cbf7a9f
Yavaaxyi <denser2006@yandex.ru>
2024-11-08 12:58:22 +0300

    08.11.24 12:58

--- register.php
+++ register.php
@@ -1,22 +1,23 @@
+
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>О компании</title>
+    <title>Регистрация</title>
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="CSS/footer.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
   </head>
   <body>
-
+    
     <!-- шапка -->
     <header>
       <div class="container d-flex flex-wrap justify-content-center">        
         <form class=" col-lg-auto mb-4 mb-lg-3" style="margin-top: auto; margin-right: auto;"  role="search">
           <input type="search" class="form-control" placeholder="Поиск по товарам" aria-label="Search">
         </form>
-        <a href="index.php" class=" d-flex align-items-center link-body-emphasis text-decoration-none" style="margin: auto;">
+        <a href="index.html" class=" d-flex align-items-center link-body-emphasis text-decoration-none" style="margin: auto;">
           <span class="fs-4"><img src="images/productcardsphoto/em.png" width="90"></span>
         </a>
         <ul class="nav" style="margin-left: auto;">
@@ -30,7 +31,7 @@
     <nav class="bg-body-tertiary border-bottom">
       <div class="container d-flex flex-wrap">
         <ul class="nav me-auto" style="margin: auto;">
-          <li class="nav-item"><a href="index.php" class="nav-link link-body-emphasis px-4"><p>ГЛАВНАЯ</p></a></li>
+          <li class="nav-item"><a href="index.html" class="nav-link link-body-emphasis px-4"><p>ГЛАВНАЯ</p></a></li>
           <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-4"><p>НОВИНКИ</p></a></li>
           <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-4"><p>МАКИЯЖ</p></a></li>
           <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-4"><p>ЛИЦО</p></a></li>
@@ -42,41 +43,48 @@
       </div>
     </nav>
     
-    <h1 class="text-center">О компании</h1>
-    
-    <!-- блок информации -->
-    <div class="container border">
-        <h3 style="text-indent: 20px;">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. 
-          Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана. 
-          Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. 
-          Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. 
-          Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни. 
-          Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики. 
-          Великий Оксмокс предупреждал ее о злых</h3>
-      </div><br>
+      <!-- форма Регистрации -->
+      <div class="container border text-center">
+        <form>
+          <div class="row mb-3">
+            <label for="inputname" class="col-sm-2 col-form-label">Имя пользователя</label>
+            <div class="col-sm-3">
+            <input type="text" class="form-control" id="inputname" placeholder="Имя пользователя">
+          </div>
+        </div>
+          <div class="row mb-3">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">E-mail</label>
+            <div class="col-sm-3">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="E-mail">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label for="inputtel" class="col-sm-2 col-form-label">Телефон</label>
+            <div class="col-sm-3">
+            <input type="tel" class="form-control" id="inputtel" placeholder="Телефон">
+          </div>
+          </div>
+          <div class="row mb-3">
+            <label for="inputPassword3" class="col-sm-2 col-form-label">Пароль</label>
+            <div class="col-sm-3">
+              <input type="password" class="form-control" id="inputPassword3" placeholder="Пароль">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label for="inputconfirmPassword3" class="col-sm-2 col-form-label">Пароль</label>
+            <div class="col-sm-3">
+              <input type="password" class="form-control" id="confirminputPassword3" placeholder="Подтвердите пароль">
+            </div>
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="personalniedannie">
+            <label class="form-check-label" for="personalniedannie">Разрешаю обработку персональных данных</label>
+          </div>
+          <a href="personal_account_dannie.html"><button type="button" class="btn btn-primary">Зарегистрироваться</button></a><br>
+          <p>Уже зарегистрированы?<a class="btn" target="_blank" href="login.html">Войти</a></p>
+        </form>
+      </div>
 
-      <!-- блок информации -->
-      <div class="container border">
-        <h3 style="text-indent: 20px;">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. 
-          Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана. 
-          Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. 
-          Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. 
-          Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни. 
-          Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики. 
-          Великий Оксмокс предупреждал ее о злых</h3>
-      </div><br>
-
-      <!-- блок информации -->
-      <div class="container border">
-        <h3 style="text-indent: 20px;">Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. 
-          Вдали от всех живут они в буквенных домах на берегу Семантика большого языкового океана. 
-          Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами. 
-          Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. 
-          Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни. 
-          Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики. 
-          Великий Оксмокс предупреждал ее о злых</h3>
-      </div><br>
-      
         <!-- футер -->
         <footer class="border-top">
           <!-- <div class="footer"> -->
@@ -150,6 +158,6 @@
           </div>
           <!--Content ends-->
         </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
